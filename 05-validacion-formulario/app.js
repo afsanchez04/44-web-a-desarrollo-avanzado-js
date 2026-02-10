eef9421efaecb35328057682246f6fe958f5916a
afsaanchez04 <afsanchez04@gmail.com>
2026-02-09 21:32:45 -0500

    validación contraseña con regex

--- 05-validacion-formulario/app.js
+++ 05-validacion-formulario/app.js
@@ -7,6 +7,15 @@ form.addEventListener("submit", (event) => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   const errorEmail = document.getElementById("errorEmail");
+  const specialCharRegex2 = /[^\w\s]/;
+  const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
+
+
+  if (!name || !email || !password) {
+    errorEmail.textContent = "Please fill in all fields.";
+    errorEmail.style.color = "orange";
+    return;
+  }
 
   if (!email.includes("@")) {
     errorEmail.textContent = "Introduce un correo electrónico válido";
@@ -15,26 +24,33 @@ form.addEventListener("submit", (event) => {
     return;
   }
 
+  //Añadir una nueva condición donde se valide contraseña
+  if (!(password.length >= 5 && specialCharRegex.test(password))) {
+    errorEmail.textContent = "Introduce una contraseña válida";
+    errorEmail.style.color = "yellow";
+    return;
+  }
+
   const userData = {
     name,
     email,
     password,
-  }
+  };
 
   errorEmail.textContent = "";
 
   fetch("https://jsonplaceholder.typicode.com/posts", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(userData)
+    body: JSON.stringify(userData),
   })
-  .then( response => response.json() )
-  .then( data => console.log(data) )
-  .catch ( error => console.error("Error:",error) )
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error:", error));
 
-  form.reset()
+  form.reset();
 
   console.log(userData);
 });
